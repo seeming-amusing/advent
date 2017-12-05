6db5b5c941edcbcc456a084d717d486da6b2b358
Eric Lee <eric.m.lee@deliveryhero.com>
2017-12-05 22:09:01 +0100

    Day 5

---
+++ 5/advent5-2.py
@@ -0,0 +1,23 @@
+import sys
+
+def readFile():
+  fileName = sys.argv[1]
+  with open(fileName) as f:
+    content = f.readlines()
+  return [int(line.rstrip('\n')) for line in content]
+
+def solve(instructions):
+  steps = 0
+  index = 0
+  while 0 <= index and index < len(instructions):
+    steps += 1
+    jump = instructions[index]
+    if jump > 2:
+      instructions[index] -= 1
+    else:
+      instructions[index] += 1
+    index += jump
+  print steps
+
+instructions = readFile()
+solve(instructions)
