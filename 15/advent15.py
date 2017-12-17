2b9d1336f05beafa25ce2ab88e0ad68fae6096c8
Eric Lee <eric.m.lee@deliveryhero.com>
2017-12-17 14:12:46 +0100

    Day 15 (improved)

--- 15/advent15.py
+++ 15/advent15.py
@@ -1,32 +1,27 @@
-class Generator:
-  def __init__(self, factor, critera, start):
-    self.factor = factor
-    self.critera = critera
-    self.value = start
-  def next(self):
-    self.value = (self.factor * self.value) % divisor
-    while self.value % self.critera != 0:
-      self.value = (self.factor * self.value) % divisor
-    return '{0:032b}'.format(self.value)
-
 divisor = 2147483647
 
 def part_1():
-  a = Generator(16807, 1, 703)
-  b = Generator(48271, 1, 516)
+  a = generator(16807, 1, 703)
+  b = generator(48271, 1, 516)
   duel(a, b, 40_000_000)
 
 def part_2():
-  a = Generator(16807, 4, 703)
-  b = Generator(48271, 8, 516)
+  a = generator(16807, 4, 703)
+  b = generator(48271, 8, 516)
   duel(a, b, 5_000_000)
 
 def duel(a, b, times):
   matches = 0
   for i in range(times):
-    if a.next()[16:] == b.next()[16:]:
+    if next(a) == next(b):
       matches += 1
   print(matches)
+
+def generator(factor, critera, value):
+  while True:
+    value = (value * factor) % divisor
+    if value % critera == 0:
+      yield value & 0xffff
 
 part_1()
 part_2()
